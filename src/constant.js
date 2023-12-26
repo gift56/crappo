814d8090a3f136bdc3138d59d6d6cfd852e899ba
gift56 <efeasiughu@gmail.com>
2023-12-26 16:58:24 -0500

    working on component creation

--- src/constant.js
+++ src/constant.js
@@ -5,7 +5,7 @@ export const rateInfoData = [
     detail: "Digital Currency Exchanged",
   },
   {
-    icon: "/icon/graphIcon.svg",
+    icon: "/icon/userIcon.svg",
     figure: "30",
     detail: "Digital Currency Exchanged",
   },
