e426c1f270f4b91eb59e8eab2f3101f02b4aeb03
gift56 <efeasiughu@gmail.com>
2023-12-26 16:43:24 -0500

    working on setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3671E9",
+        dark: "#0D0D2B",
+        gray1: "#ffffff1a",
+        textgray: "#E0E0E0",
+        textgray2: "#828282",
+      },
+      backgroundImage: {
+        heroLinear:
+          "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0.49%, rgba(255, 255, 255, 0.04) 39.06%, rgba(255, 255, 255, 0.00) 99.92%)",
+      },
+    },
   },
   plugins: [],
 };
