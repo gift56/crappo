3e1b3e16b5625b82130038df80f97aae00c3a791
gift56 <efeasiughu@gmail.com>
2023-12-27 17:56:56 -0500

    working on the next

--- src/components/Choice.vue
+++ src/components/Choice.vue
@@ -20,8 +20,55 @@
         <div
           class="w-full flex flex-col lg:flex-row items-center justify-center gap-8 md:gap-12"
         >
-      
-      </div>
+          <div
+            class="w-full lg:flex-1 flex flex-col items-start justify-start gap-6"
+          >
+            <h2
+              v-motion
+              :initial="{ opacity: 0, y: 50 }"
+              :visible="{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 100,
+                },
+              }"
+              class="text-2xl font-bold md:text-3xl md:leading-[150%]"
+            >
+              Invest Smart
+            </h2>
+            <p
+              v-motion
+              :initial="{ opacity: 0, y: 50 }"
+              :visible="{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 100,
+                },
+              }"
+              class="font-normal text-base text-textgray"
+            >
+              Get full statistic information about the behaviour of buyers and
+              sellers will help you to make the decision.
+            </p>
+            <button
+              v-motion
+              :initial="{ opacity: 0, y: 50 }"
+              :visible="{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 50,
+                },
+              }"
+              type="button"
+              class="rounded-full w-fit px-8 py-4 outline-none flex items-center justify-center gap-4 border-2 border-primary text-white bg-primary text-sm sm:text-base font-medium capitalize hover:bg-primary/60 group transition-all duration-300"
+            >
+              Learn More
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </section>
