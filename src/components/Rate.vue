aecf803edc1aa60cbec36a570c05fe5185d2f1c3
gift56 <efeasiughu@gmail.com>
2023-12-27 15:39:46 -0500

    working on the next

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -2,14 +2,17 @@
   <section class="w-full">
     <div class="container">
       <div
-        class="w-full py-12 flex flex-wrap lg:flex-nowrap items-center justify-center gap-8 md:gap-12"
+        class="w-full py-12 flex flex-wrap lg:flex-nowrap items-center justify-start gap-8 md:gap-12"
       >
-        <img
-          :key="rate.detail"
-          :src="rate.icon"
-          :alt="rate.detail"
+        <div
           v-for="rate in rateInfoData"
-        />
+          class="w-fit flex flex-col md:flex-row items-start justify-start gap-3"
+        >
+          <img :key="rate.detail" :src="rate.icon" :alt="rate.detail" />
+          <div class="flex flex-col items-start">
+            <h3></h3>
+          </div>
+        </div>
       </div>
     </div>
   </section>
