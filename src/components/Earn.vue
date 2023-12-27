d2bfc93ce9a6fa06e173a591b9a655ea11fe9750
gift56 <efeasiughu@gmail.com>
2023-12-27 16:02:56 -0500

    working on the next

--- src/components/Earn.vue
+++ src/components/Earn.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="w-full relative">
     <div class="container">
-      
+      <div class="w-full flex items-center justify-center flex-col gap-8 pt-12">
+
+      </div>
     </div>
   </section>
 </template>
