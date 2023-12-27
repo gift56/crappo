0a96952b746e7fb9aa717506cfeff50b7f884094
gift56 <efeasiughu@gmail.com>
2023-12-27 17:29:38 -0500

    working on the next

--- src/components/Currencies.vue
+++ src/components/Currencies.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="w-full bg-white">
     <div class="container py-40">
-      <div class="w-full flex items-center justify-center flex-col gap-10 pt-32">
+      <div
+        class="w-full flex items-center justify-center flex-col gap-10 pt-32"
+      >
         <h2
           v-motion
           :initial="{ opacity: 0, y: 50 }"
@@ -12,7 +14,7 @@
               delay: 100,
             },
           }"
-          class="text-3xl font-bold md:text-4xl md:leading-[150%] text-center text-dark"
+          class="text-2xl sm:text-3xl font-bold md:text-4xl md:leading-[150%] text-center text-dark"
         >
           Trade securely and market the high <br class="hidden lg:block" />
           growth cryptocurrencies.
@@ -33,8 +35,22 @@
                 delay: 100 * index,
               },
             }"
-            class="group w-full flex flex-col items-center justify-center gap-6 p-4 md:p-6 bg-white hover:bg-secondary transition-all duration-200 rounded-2xl shadow-exchangeShad mix-blend-overlay"
-          ></div>
+            class="group w-full flex flex-col items-center justify-center gap-6 p-4 md:p-6 bg-white hover:bg-secondary transition-all duration-200 rounded-2xl shadow-exchangeShad cursor-pointer"
+          >
+            <img
+              :src="item.coinIcon"
+              :alt="item.coinName"
+              class="w-12 h-12 md:w-20 md:h-20 object-contain rounded-full"
+            />
+            <h4
+              class="flex items-start justify-center gap-2 text-2xl sm:text-3xl font-bold text-dark group-hover:text-white"
+            >
+              <span>{{ item.coinName }}</span>
+              <span class="text-base font-medium text-[#BDBDBD] group-hover:text-white/70 md:text-lg">{{
+                item.abbrevate
+              }}</span>
+            </h4>
+          </div>
         </div>
       </div>
     </div>
