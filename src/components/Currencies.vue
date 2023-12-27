ead41ef81112449b5b34ad7a5894d507091b254f
gift56 <efeasiughu@gmail.com>
2023-12-27 16:53:33 -0500

    working on the next

--- src/components/Currencies.vue
+++ src/components/Currencies.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="w-full bg-white">
     <div class="container py-40">
-
+      <div class="w-full flex items-center justify-center flex-col pt-12">
+        
+      </div>
     </div>
   </section>
 </template>
