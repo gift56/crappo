95496f9d12d0d147e47f330789723d8c7c8100fb
gift56 <efeasiughu@gmail.com>
2023-12-27 15:36:06 -0500

    working on the next

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -2,8 +2,10 @@
   <section class="w-full">
     <div class="container">
       <div
-        class="w-full py-12 flex flex-col lg:flex-row items-center justify-center gap-8 md:gap-12"
-      ></div>
+        class="w-full py-12 flex flex-wrap lg:flex-nowrap items-center justify-center gap-8 md:gap-12"
+      >
+    
+    </div>
     </div>
   </section>
 </template>
