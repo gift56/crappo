8c4cdb8e0d436e1d0ba0cd6933b21d2f595ae781
gift56 <efeasiughu@gmail.com>
2023-12-27 17:53:09 -0500

    working on the next

--- src/components/Choice.vue
+++ src/components/Choice.vue
@@ -1,5 +1,13 @@
 <template>
-  <section class="w-full">Choice</section>
+  <section class="w-full bg-secondary">
+    <div class="container py-14">
+      <div
+        class="w-full flex items-center justify-center flex-col gap-10"
+      >
+    
+    </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
