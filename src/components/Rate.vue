82727f7d5b51808533068f857624ccb45edaccdd
gift56 <efeasiughu@gmail.com>
2023-12-27 15:45:20 -0500

    working on the next

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -6,6 +6,16 @@
       >
         <div
           v-for="(rate, index) in rateInfoData"
+          v-motion
+          :initial="{ opacity: 0, scale: 0, y: 50 }"
+          :visible="{
+            opacity: 1,
+            scale: 1,
+            y: 0,
+            transition: {
+              delay: 100 * index,
+            },
+          }"
           class="w-fit flex flex-col md:flex-row items-start justify-start gap-3"
         >
           <img :key="rate.detail" :src="rate.icon" :alt="rate.detail" />
