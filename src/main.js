c85cc46971563eeeb12cd34c9557e6b2ce3dd8f9
gift56 <efeasiughu@gmail.com>
2023-12-27 19:28:50 -0500

    working on the line chart

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { MotionPlugin } from "@vueuse/motion";
+import CanvasJSChart from '@canvasjs/vue-charts';
 
 const app = createApp(App);
 
