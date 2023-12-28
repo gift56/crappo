f56875f6f74063970a48fa5d9b97a415891a8a8c
gift56 <efeasiughu@gmail.com>
2023-12-28 18:23:35 -0500

    working on investment section

--- src/components/Investment.vue
+++ src/components/Investment.vue
@@ -3,7 +3,9 @@
     <div class="container py-40">
       <div
         class="w-full flex flex-col lg:flex-row items-center justify-center gap-8 md:gap-12"
-      ></div>
+      >
+    
+    </div>
     </div>
   </section>
 </template>
