7947e70d71e2ded48217062b18a88485528572f3
gift56 <efeasiughu@gmail.com>
2023-12-29 06:55:39 -0500

    working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,19 @@
   <footer class="bg-dark">
     <div class="container py-10">
       <div class="w-full flex flex-col items-start justify-start gap-10">
-
-        
+        <div
+          class="w-full flex flex-col md:flex-row gap-6 items-start justify-between"
+        >
+          <div
+            class="flex flex-col items-start justify-start w-full sm:w-[50%]"
+          >
+            <RouterLink to="/" class="select-none">
+              <h2 class="font-dm text-2xl font-bold text-primary md:text-2xl">
+                banquee.
+              </h2>
+            </RouterLink>
+          </div>
+        </div>
       </div>
     </div>
   </footer>
