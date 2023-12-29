d7fe425ef9a17ddc0d32d04e584514f892b78c5a
gift56 <efeasiughu@gmail.com>
2023-12-29 06:54:35 -0500

    working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,10 @@
 <template>
   <footer class="bg-dark">
     <div class="container py-10">
+      <div class="w-full flex flex-col items-start justify-start gap-10">
 
-      
+        
+      </div>
     </div>
   </footer>
 </template>
