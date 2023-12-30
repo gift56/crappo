4c120c9ab2ebdcf9219c16e61c8120b106566038
gift56 <efeasiughu@gmail.com>
2023-12-30 18:21:40 -0500

    working on adding more coin

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,42 @@ import coinbaseWalletModule from "@web3-onboard/coinbase";
 import walletConnectModule from "@web3-onboard/walletconnect";
 
 const infuraKey = import.meta.env.VITE_INFURA_KEY;
+
 const rpcUrl = `https://mainnet.infura.io/v3/${infuraKey}`;
+
 const injected = injectedModule({
   filter: {
     // allow only on non android mobile
     [ProviderLabel.Detected]: ["Android", "desktop"],
   },
 });
-const coinbaseWalletModule = coinbaseWalletModule();
-const walletConnect = walletConnectModule();
+
+const wcInitOptions = {
+  /**
+   * Project ID associated with [WalletConnect account](https://cloud.walletconnect.com)
+   */
+  projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+  /**
+   * Chains required to be supported by all wallets connecting to your DApp
+   */
+  requiredChains: [1],
+  /**
+   * Chains required to be supported by all wallets connecting to your DApp
+   */
+  optionalChains: [42161, 8453, 10, 137, 56],
+  /**
+   * Defaults to `appMetadata.explore` that is supplied to the web3-onboard init
+   * Strongly recommended to provide atleast one URL as it is required by some wallets (i.e. MetaMask)
+   * To connect with WalletConnect
+   */
+  dappUrl: import.meta.env.VITE_WALLET_APP_URL,
+};
+
+const coinbaseWallet = coinbaseWalletModule();
+const walletConnect = walletConnectModule(wcInitOptions);
 
 init({
-  wallets: [coinbaseWalletModule, walletConnect, injected],
+  wallets: [coinbaseWallet, walletConnect, injected],
   chains: [
     {
       id: "0x1",
@@ -45,9 +69,17 @@ init({
     },
     {
       id: "0x3",
-      token: "rETH",
-      label: "Ethereum Ropsten",
+      token: "tROP",
+      namespace: "evm",
+      label: "Ethereum Ropsten Testnet",
       rpcUrl: `https://ropsten.infura.io/v3/${infuraKey}`,
+    },
+    {
+      id: "0x4",
+      token: "rETH",
+      namespace: "evm",
+      label: "Ethereum Rinkeby Testnet",
+      rpcUrl: `https://rinkeby.infura.io/v3/${infuraKey}`,
     },
   ],
 });
